72a44bfbbf62fb577e8701c36501790e73d877e4
JanineCruz921 <cruzjanine921@gmail.com>
2023-10-04 15:54:35 -0300

    inicio do sistema js da página donation

---
+++ src/pages/donation/js/script.js
@@ -0,0 +1,20 @@
+var btnDonate = document.querySelector("#btnDonate");
+var btnDonateValue = document.querySelector("#btnDonateValue");
+var btnDonateFurniture = document.querySelector("#btnDonateFurniture");
+var btnAssociation = document.querySelector("#btnAssociation");
+
+function skipTopageDonate (){
+
+}
+
+function skipToPageDonateValue(){
+
+}
+
+function skipToPageDonateForniture(){
+
+}
+
+function skipToPageAssociation(){
+    
+}
