623a5ad0a86a16ed4b3b6b03a0d5414619372127
JanineCruz921 <cruzjanine921@gmail.com>
2023-10-04 20:41:33 -0300

    ajustes na qualidade das imagens, correções de sintaxe, ínicio do medea quieres

--- src/pages/donation/js/script.js
+++ src/pages/donation/js/script.js
@@ -2,19 +2,3 @@ var btnDonate = document.querySelector("#btnDonate");
 var btnDonateValue = document.querySelector("#btnDonateValue");
 var btnDonateFurniture = document.querySelector("#btnDonateFurniture");
 var btnAssociation = document.querySelector("#btnAssociation");
-
-function skipTopageDonate (){
-
-}
-
-function skipToPageDonateValue(){
-
-}
-
-function skipToPageDonateForniture(){
-
-}
-
-function skipToPageAssociation(){
-    
-}
