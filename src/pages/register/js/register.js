e0b607caaed03b8a7d05ec0a04a18e468fb56ff7
Paulo Ricardo M <pr741234@gmail.com>
2023-10-11 02:15:06 -0300

    :sparkles: feat: feito validação do formulário de registro

--- src/pages/register/js/register.js
+++ src/pages/register/js/register.js
@@ -1,21 +1,61 @@
-const form = document.getElementById("form");
-const firstNameInput = document.getElementById("first_name");
-const lastNameInput = document.getElementById("last_name");
-const emailInput = document.getElementById("email");
-const phoneNumberInput = document.getElementById("phone_number");
-const cepInput = document.getElementById("CEP");
-const addressInput = document.getElementById("address");
-const neighborhoodInput = document.getElementById("neighborhood");
-const cityInput = document.getElementById("city");
-const stateInput = document.getElementById("state");
-const houseNumberInput = document.getElementById("house_number");
-const storyInput = document.getElementById("story");
+const formElement = document.getElementById("form");
+const inputElements = document.querySelectorAll("input");
 
-form.addEventListener("submit", (e) => {
+const validationMessages = {
+  email: "O e-mail deve conter um '@' e um '.'",
+  phone_number: "O número deve ter o formato (xx) xxxxx-xxxx.",
+};
+
+formElement.addEventListener("submit", (e) => {
   e.preventDefault();
   validateForm();
 });
 
+const updateElementStatus = (element, status, messageError = "") => {
+  const inputContainer = element.parentElement;
+  const invalidMessage = inputContainer.querySelector(".invalid_message");
+  inputContainer.classList.add(status);
+  inputContainer.classList.remove(status === "success" ? "invalid" : "success");
+  invalidMessage.style.display = status === "success" ? "none" : "inline";
+  if (messageError) {
+    invalidMessage.innerText = messageError;
+  }
+};
+
+const validateEmail = (input) => {
+  const emailRegex = /^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$/;
+  return emailRegex.test(input.value.trim());
+};
+
+const validatePhoneNumber = (input) => {
+  const phoneRegex = /^\(\d{2}\) \d{5}-\d{4}$/;
+  return phoneRegex.test(input.value.trim());
+};
+
 const validateForm = () => {
-  const firstNameValue = firstNameInput.value.trim()
+  let isFormValid = true;
+
+  inputElements.forEach((input) => {
+    const value = input.value.trim();
+
+    if (value === "") {
+      updateElementStatus(input, "invalid");
+      isFormValid = false;
+    } else if (input.id === "email" && !validateEmail(input)) {
+      updateElementStatus(input, "invalid", validationMessages.email);
+      isFormValid = false;
+    } else if (input.id === "phone_number" && !validatePhoneNumber(input)) {
+      updateElementStatus(input, "invalid", validationMessages.phone_number);
+      isFormValid = false;
+    } else {
+      updateElementStatus(input, "success");
+    }
+  });
+
+  if (isFormValid) {
+    formElement.submit();
+    alert(
+      "Cadastro enviado! estamos ansiosos para ajudá-lo a transformar a esperança em realidade e criar um lar para você e sua família. Entraremos em contato em breve com mais informações."
+    );
+  }
 };
